7fd1638033f7073ff361c424d99d9a526e614f54
janmusial <jasiolix@gmail.com>
2024-05-06 17:08:33 +0200

    Added Post method for adding new visit and Get method for getting visits for animal with specified id

--- src/RestVetClinic/RestVetClinic/RestVetClinic/Program.cs
+++ src/RestVetClinic/RestVetClinic/RestVetClinic/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IAnimalRepository, AnimalRepository>();
+builder.Services.AddSingleton<IVisitRepository, VisitRepository>();
 
 var app = builder.Build();
 
@@ -22,7 +23,7 @@ app.UseHttpsRedirection();
 
 app.MapPost("/animals", (IAnimalRepository animalRepository, Animal animal) => animalRepository.Add(animal)
     ? Results.Created()
-    : Results.BadRequest($"Animal with id {animal.Id} already exists")).WithName("AddAnimal").WithOpenApi();
+    : Results.BadRequest($"Animal with id {animal.Id} already exists.")).WithName("AddAnimal").WithOpenApi();
 
 
 app.MapGet("/animals", (IAnimalRepository animalRepository) => Results.Ok(animalRepository.GetAll()))
@@ -32,12 +33,12 @@ app.MapGet("/animals", (IAnimalRepository animalRepository) => Results.Ok(animal
 app.MapGet("/animals/{id:int}", (IAnimalRepository animalRepository, int id) =>
 {
     var animal = animalRepository.Get(id);
-    return (animal == null) ? Results.NotFound($"Animal with id {id} was not found") : Results.Ok(animal);
+    return (animal == null) ? Results.NotFound($"Animal with id {id} was not found.") : Results.Ok(animal);
 }).WithName("GetAnimal").WithOpenApi();
 
 app.MapDelete("/animals/{id:int}", (IAnimalRepository animalRepository, int id) => animalRepository.Delete(id)
     ? Results.NoContent()
-    : Results.BadRequest($"Animal with id {id} was not found")).WithName("DeleteAnimal").WithOpenApi();
+    : Results.NotFound($"Animal with id {id} was not found.")).WithName("DeleteAnimal").WithOpenApi();
 
 app.MapPut("/animals/{id:int}", (IAnimalRepository animalRepository, int id, Animal animal) =>
         animalRepository.Edit(id, animal)
@@ -45,5 +46,15 @@ app.MapPut("/animals/{id:int}", (IAnimalRepository animalRepository, int id, Ani
             : Results.BadRequest(
                 $"Animal with id {id} was not found or the id in url segment doesn't match the id in http body."))
     .WithName("EditAnimal").WithOpenApi();
+    
+app.MapPost("/visits", (IAnimalRepository animalRepository, IVisitRepository visitRepository, Visit visit) => visitRepository.Add(animalRepository, visit)
+    ? Results.Created()
+    : Results.NotFound($"Animal with id {visit.AnimalId} was not found.")).WithName("AddVisit").WithOpenApi();
+
+app.MapGet("/visits/{animalId:int}", (IAnimalRepository animalRepository,  IVisitRepository visitRepository, int animalId) =>
+{
+    var visit = visitRepository.GetForAnimal(animalRepository, animalId);
+    return (visit == null) ? Results.NotFound($"Animal with id {animalId} was not found.") : Results.Ok(visit);
+}).WithName("GetVisit").WithOpenApi();
 
 app.Run();
