ca8ea00708c0a475f43ff1dc09d46bb2cb8054b5
janmusial <jasiolix@gmail.com>
2024-05-06 17:01:03 +0200

    Modified Visit to have property int AnimalId instead of Animal object

--- src/RestVetClinic/RestVetClinic/RestVetClinic/Models/Visit.cs
+++ src/RestVetClinic/RestVetClinic/RestVetClinic/Models/Visit.cs
@@ -3,7 +3,7 @@ namespace RestVetClinic.Models;
 public class Visit
 {
     public DateTime Date { get; set; }
-    public Animal Animal { get; set; }
+    public int AnimalId { get; set; }
     public string Description { get; set; }
     public double price { get; set; }
 }
--- src/RestVetClinic/RestVetClinic/RestVetClinic/Repositories/VisitRepository.cs
+++ src/RestVetClinic/RestVetClinic/RestVetClinic/Repositories/VisitRepository.cs
@@ -8,13 +8,13 @@ public class VisitRepository : IVisitRepository
 
     public bool Add(IAnimalRepository animalRepository, Visit visit)
     {
-        if (animalRepository.Get(visit.Animal.Id) == null) return false;
+        if (animalRepository.Get(visit.AnimalId) == null) return false;
         _visits.Add(visit);
         return true;
     }
 
     public IEnumerable<Visit>? GetForAnimal(IAnimalRepository animalRepository, int animalId)
     {
-        return animalRepository.Get(animalId) == null ? null : _visits.FindAll(visit => visit.Animal.Id == animalId);
+        return animalRepository.Get(animalId) == null ? null : _visits.FindAll(visit => visit.AnimalId == animalId);
     }
 }
